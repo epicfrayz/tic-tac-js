b6a955707a6ea6d693fecd478e9583cf214b0051
PromiSe#### <vic@iMac-PromiSe.local>
2020-11-01 20:03:32 +0300

    Компонент игры

---
+++ components/Game.tsx
@@ -0,0 +1,25 @@
+import React, { FC, useState } from "react";
+
+import "./Game.sass";
+
+import { GameViewComponent } from "./GameView";
+import { MenuViewComponent } from "./MenuView";
+
+export const ViewModels = {
+  menu: MenuViewComponent,
+  game: GameViewComponent
+}
+
+export type TViewMode = keyof typeof ViewModels
+export type TViewFC = FC<{setView(v: TViewMode): void}>
+
+export const GameComponent = () => {
+  const [model, setView] = useState<TViewMode>('menu')
+  const View = ViewModels[model]
+
+  return (
+    <div className="game">
+      <View setView={setView} />
+    </div>
+  )
+}
