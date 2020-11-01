807c80f354b4f41b18801095fcd76101194ab7e9
PromiSe#### <vic@iMac-PromiSe.local>
2020-11-01 20:29:36 +0300

    Вывод статуса ничья

--- components/GamePopup.tsx
+++ components/GamePopup.tsx
@@ -3,14 +3,21 @@ import { ButtonComponent } from "./Button";
 
 import "./GamePopup.sass";
 
-export const GamePopupComponent = ({win = 0, onClick = null}) => {
+export const WinState = (win = 0) => {
+  switch (win) {
+    case 0: return 'Ничья'
+    case 1: return 'Вы выиграли'
+    case 2: return 'Вы проиграли'
+  }
+}
 
-  if(!win)
+export const GamePopupComponent = ({win = 0, onClick = null}) => {
+  if(win === null)
     return null
 
   return (
-    <div className={`game-popup ${win == 1 ? 'win' : ''}`}>
-      <p>Вы {win == 1 ? 'выиграли' : 'проиграли'}</p>
+    <div className="game-popup">
+      <p>{WinState(win)}</p>
       <ButtonComponent type="ok" onClick={onClick} />
     </div>
   )
