250c21bd01722d091bbc1d25895f41955c83dfd4
PromiSe#### <vic@iMac-PromiSe.local>
2020-11-01 20:03:54 +0300

    Компонент вида игры

---
+++ components/GameView.tsx
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import { checkWin, getPlayer } from "../lib/TicTac";
+import { ButtonComponent } from "./Button";
+import { GamePopupComponent } from "./GamePopup";
+import { TViewFC } from "./Game";
+
+import "./GameView.sass"
+import { statStore } from "./MenuView";
+
+export const GameViewComponent: TViewFC = ({ setView }) => {
+  const [state, setState] = useState([...new Uint8Array(9)])
+  const [win, setWin] = useState(0)
+  const player = getPlayer(state)
+
+  const handleClick = (index = -1) => {
+    if (win)
+      return null
+
+    if (typeof state[index] == 'undefined')
+      return null
+
+    const newState = state.map((e, i) =>
+      i == index ? player : e)
+
+    const findWin = checkWin(newState)
+
+    let { wins, deads } = statStore.get()
+
+    if (findWin) {
+      if (findWin == 1)
+        wins += 1
+      else
+        deads += 1
+
+      statStore.set({ wins, deads })
+      setWin(findWin)
+    }
+
+    setState(newState)
+  }
+
+  return (
+    <div className="game-container-grid">
+      {state.map((e, i) =>
+        <ButtonComponent
+          key={`btn-${i}`}
+          onClick={!e ? () => handleClick(i) : null}
+          type={e ? (e == 1 ? 'x-value' : 'o-value') : 'default'} />
+      )}
+      <GamePopupComponent onClick={() => setView('menu')} win={win} />
+    </div>
+  )
+}
