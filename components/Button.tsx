1a2d67dcef7e0041ad2a9c3f647df041a74631a8
PromiSe#### <vic@iMac-PromiSe.local>
2020-11-02 07:26:38 +0300

    Добавил параметр прозрачности

--- components/Button.tsx
+++ components/Button.tsx
@@ -5,13 +5,15 @@ import "./Button.sass";
 interface IButtonProps {
   type?: 'refresh' | 'x-value' | 'o-value' | 'default' | 'ok',
   onClick?(e: MouseEvent): void
+  opacity?: number
 }
 
 export const ButtonComponent: FC<IButtonProps> = (props) => {
-  const { type = 'default', onClick } = props
+  const { type = 'default', onClick, opacity = 1 } = props
   const className = `btn ${type} ${onClick ? 'clicked' : ''}`
   return (
     <button 
+      style={{opacity}}
       onClick={onClick as any}
       className={className} />
   )
