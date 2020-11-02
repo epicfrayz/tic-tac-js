547fcc5947a906a6f1f5aafc4e61a34094e88650
PromiSe#### <vic@iMac-PromiSe.local>
2020-11-02 19:20:47 +0300

    Добавил перенос

--- components/Button.tsx
+++ components/Button.tsx
@@ -11,6 +11,7 @@ interface IButtonProps {
 export const ButtonComponent: FC<IButtonProps> = (props) => {
   const { type = 'default', onClick, opacity = 1 } = props
   const className = `btn ${type} ${onClick ? 'clicked' : ''}`
+  
   return (
     <button 
       style={{opacity}}
